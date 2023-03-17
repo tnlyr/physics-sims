762f124628f5abf2b00ae8fff71be38cff15142b
L.A <40433952+tnlyr@users.noreply.github.com>
2023-03-17 19:48:44 -0400

    add: Main Window button handling

--- src/main/java/edu/vanier/physicsimulations/controllers/MainAppController.java
+++ src/main/java/edu/vanier/physicsimulations/controllers/MainAppController.java
@@ -1,10 +1,26 @@
 package edu.vanier.physicsimulations.controllers;
 
-public class MainAppController {
-    public MainAppController() {
-    }
+import edu.vanier.physicsimulations.views.OpticsApp;
+import edu.vanier.physicsimulations.views.ProjectileApp;
+import edu.vanier.physicsimulations.views.ShmApp;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MainAppController {
+    @FXML
+    Button projectileBtn, shmBtn, opticsBtn;
+    @FXML
     private void initialize() {
+        projectileBtn.setOnAction(e -> {
+            new ProjectileApp();
+        });
+        opticsBtn.setOnAction(e -> {
+            new OpticsApp();
+        });
+        shmBtn.setOnAction(e -> {
+            new ShmApp();
+        });
     }
 
 }
