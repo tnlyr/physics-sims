2b089be8f109035d59ea0c5e3c65c7994c49cc36
L.A <40433952+tnlyr@users.noreply.github.com>
2023-03-17 19:48:56 -0400

    add: Main Views for Subwindows

--- src/main/java/edu/vanier/physicsimulations/views/OpticsApp.java
+++ src/main/java/edu/vanier/physicsimulations/views/OpticsApp.java
@@ -1,7 +1,30 @@
 package edu.vanier.physicsimulations.views;
 
-public class OpticsApp {
-    public void start() {
+import edu.vanier.physicsimulations.controllers.OpticsController;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class OpticsApp extends Stage {
+    public OpticsApp() {
+        this.setTitle("Optics Simulation");
+        this.initModality(Modality.APPLICATION_MODAL);
+        try {
+            start();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void start() throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Optics.fxml"));
+        loader.setController(new OpticsController());
+        Pane root = loader.load();
+        this.setScene(new Scene(root));
+        this.sizeToScene();
+        this.show();
     }
 }
--- src/main/java/edu/vanier/physicsimulations/views/ProjectileApp.java
+++ src/main/java/edu/vanier/physicsimulations/views/ProjectileApp.java
@@ -1,7 +1,31 @@
 package edu.vanier.physicsimulations.views;
 
-public class ProjectileApp {
-    public void start() {
+import edu.vanier.physicsimulations.controllers.ProjectileMotionController;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class ProjectileApp extends Stage {
+
+    public ProjectileApp() {
+        this.setTitle("Projectile Simulation");
+        this.initModality(Modality.APPLICATION_MODAL);
+        try {
+            start();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void start() throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Projectile.fxml"));
+        loader.setController(new ProjectileMotionController());
+        Pane root = loader.load();
+        this.setScene(new Scene(root));
+        this.sizeToScene();
+        this.show();
     }
 }
