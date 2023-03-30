748065dfe5a33609f23232df7d5ab63d3e7ccb19
Shtrust <104174257+Shtrust@users.noreply.github.com>
2023-03-30 19:18:44 -0400

    Set fx:id's with @FXML tags in SHMcontroller class

--- src/main/java/edu/vanier/physicsimulations/controllers/ShmController.java
+++ src/main/java/edu/vanier/physicsimulations/controllers/ShmController.java
@@ -1,7 +1,40 @@
 package edu.vanier.physicsimulations.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
+import javafx.scene.control.Spinner;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
+
 public class ShmController {
+
+    @FXML
+    private Spinner lengthSpinner;
+    @FXML
+    private Spinner angleSpinner;
+    @FXML
+    private Spinner massSpinner;
+    @FXML
+    private Spinner gravitySpinner;
+    @FXML
+    private Slider playbackSlider;
+    @FXML
+    private Circle topCircle;
+    @FXML
+    private Line string;
+    @FXML
+    private Rectangle rectangle;
+    @FXML
+    private Button playBtn;
+    @FXML
+    private Button resetBtn;
+
+
     public ShmController() {
+
+
     }
 
     private void initialize() {
