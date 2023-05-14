a6f746bec03e8bdc60576af1620c3190abbecaf2
L.A <40433952+tnlyr@users.noreply.github.com>
2023-05-14 18:17:30 -0400

    update(module-info) : supports media

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module edu.vanier.physicsimulations {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.media;
 
     exports edu.vanier.physicsimulations.views;
     exports edu.vanier.physicsimulations.engines to javafx.fxml;
